90a9abb34ee8414652a0f7bdfa129689dcadcc20
Federico Goncalvez <129781442+FedericoGoncalvez@users.noreply.github.com>
2023-05-05 17:36:28 -0300

    Se agregan las pages

---
+++ src/test/java/pages/MyAccountPage.java
@@ -0,0 +1,46 @@
+package pages;
+
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import static org.testng.Assert.assertEquals;
+
+public class MyAccountPage extends BasePage{
+
+    public MyAccountPage(WebDriver driver) {
+        super(driver);
+    }
+
+
+    @FindBy(className = "hello")
+    WebElement assertLogin;
+
+    @FindBy(className = "logo")
+    WebElement homePageButton;
+
+    @Step("Se obtiene el título para realizar el assert.")
+    public String assertLoginPage(){
+
+        String getTitle = assertLogin.getText();
+        return getTitle;
+
+    }
+
+    @Step("Se pasa el título para realizar el assert.")
+    public String titlePage(){
+
+        String title = "Hello, Test Account!";
+        return title;
+
+    }
+
+    @Step("Se regresa a la HomePage.")
+    public void returnHomePage(){
+
+        homePageButton.click();
+    }
+
+}
---
+++ src/test/java/scripts/BaseTest.java
@@ -0,0 +1,47 @@
+package scripts;
+
+
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+
+    public static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp() throws Exception{
+
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+    }
+
+    /*@Attachment(type = "image/png")
+    @AfterMethod(alwaysRun = true)
+    public byte[] takeScreenshot() throws Exception {
+        byte[] image = new byte[0];
+        try {
+            TakesScreenshot screenshot = (TakesScreenshot) driver;
+            image = screenshot.getScreenshotAs(OutputType.BYTES);
+            System.out.println("Successfully captured a screenshot");
+        } catch (Exception e) {
+            System.out.println("Exception while taking screenshot " + e.getMessage());
+        }
+        return image;
+    }*/
+
+    @AfterTest
+    public void afterTest(){
+        driver.quit();
+    }
+}
