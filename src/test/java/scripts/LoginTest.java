48620c30f4b9f4be2bddfbb84fb5dfe0e434961c
Federico Goncalvez <129781442+FedericoGoncalvez@users.noreply.github.com>
2023-05-05 17:44:02 -0300

    Se agregan los parameters

--- src/test/java/scripts/LoginTest.java
+++ src/test/java/scripts/LoginTest.java
@@ -1,9 +1,32 @@
 package scripts;
 
 
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import pages.AccountLoginPage;
+import pages.LoginPage;
+import pages.MyAccountPage;
 
-public class LoginTest {
-    public static void main(String[] args) {
-        System.out.println("hola mundo");
+import static org.testng.Assert.assertEquals;
+
+
+public class LoginTest extends BaseTest{
+
+    @Parameters({"emailAccount","passAccount"})
+
+    @Test
+    public void test(String emailAccount, String passAccount){
+
+        driver.get("http://magento-demo.lexiconn.com/");
+        LoginPage loginPage = new LoginPage(driver);
+        AccountLoginPage accountLoginPage = loginPage.enterLoginPage();
+        MyAccountPage myAccountPage = accountLoginPage.enterMyAccount(emailAccount,passAccount);
+        String getTitle = myAccountPage.assertLoginPage();
+        String title = myAccountPage.titlePage();
+        assertEquals(title,getTitle);
+        myAccountPage.returnHomePage();
+
+
     }
+
 }
