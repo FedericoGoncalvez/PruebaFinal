c053eb7fdcb50da9e8ceeefa03ca529f9ad0a512
Federico Goncalvez <129781442+FedericoGoncalvez@users.noreply.github.com>
2023-05-06 13:59:17 -0300

    Se crea el test negativo de registro

--- src/test/java/scripts/RegisterTest.java
+++ src/test/java/scripts/RegisterTest.java
@@ -1,4 +1,30 @@
 package scripts;
 
-public class RegisterTest {
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.MainPage;
+import pages.MyAccountPage;
+import pages.RegisterPage;
+
+import static org.testng.Assert.assertEquals;
+
+public class RegisterTest extends BaseTest{
+
+    @Parameters({"name","lastname","email","password", "confirm"})
+
+    @Test
+    public void test(String name, String lastname, String email, String password, String confirm){
+
+
+        driver.get("http://magento-demo.lexiconn.com/");
+        MainPage home = new MainPage(driver);
+        RegisterPage registerPage = home.enterRegisterPage();
+        registerPage.register(name, lastname, email,password, confirm);
+        String actualAdvice = registerPage.assertAdvice();
+        String expectedAdvice = "This is a required field.";
+        Assert.assertEquals(actualAdvice, expectedAdvice);
+
+    }
 }
