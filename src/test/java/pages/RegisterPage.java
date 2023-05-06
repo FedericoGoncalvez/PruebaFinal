91dce95d65b49ad3d07bf9156d399930a10e5dec
Federico Goncalvez <129781442+FedericoGoncalvez@users.noreply.github.com>
2023-05-06 12:27:34 -0300

    Se capturan los elementos de la pagina de registro

--- src/test/java/pages/RegisterPage.java
+++ src/test/java/pages/RegisterPage.java
@@ -1,9 +1,33 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class RegisterPage extends BasePage {
     public RegisterPage(WebDriver driver) {
         super(driver);
     }
+
+    @FindBy(xpath = "//input[@id='firstname']")
+    private WebElement firstNameInput;
+
+    @FindBy(xpath = "//input[@id='lastname']")
+    private WebElement lastNameInput;
+
+    @FindBy(xpath = "//input[@id='email_address']")
+    private WebElement emailInput;
+
+    @FindBy(xpath = "//input[@id='password']")
+    private WebElement passwordInput;
+
+    @FindBy(xpath = "//input[@id='confirmation']")
+    private WebElement confirmPasswordInput;
+
+    @FindBy(xpath = "//button[@title='Register']")
+    private WebElement registerButton;
+
+
 }
+
+
