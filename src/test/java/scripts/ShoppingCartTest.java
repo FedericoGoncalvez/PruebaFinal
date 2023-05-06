23fbeb14e27fb678f899e7c9552c1df02936e8fb
Federico Goncalvez <129781442+FedericoGoncalvez@users.noreply.github.com>
2023-05-06 20:48:06 -0300

    Se actualizan los selectores

--- src/test/java/scripts/ShoppingCartTest.java
+++ src/test/java/scripts/ShoppingCartTest.java
@@ -1,5 +1,27 @@
 package scripts;
 
-public class ShoppingCartTest {
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import pages.CartPage;
+import pages.ProductPage;
 
+
+import static org.testng.Assert.assertEquals;
+
+public class ShoppingCartTest extends BaseTest{
+    @Parameters({"textExpected"})
+
+    @Test
+    public void Test(String textExpected){
+
+        driver.get("http://magento-demo.lexiconn.com/");
+
+        ProductPage productPage = new ProductPage(driver);
+        productPage.selectCatalog();
+        productPage.selectItems();
+        productPage.clickAddToCartButton();
+        CartPage cartPage = new CartPage(driver);
+        String productAdd = cartPage.getTextFromElement();
+        assertEquals(productAdd, textExpected);
+    }
 }
