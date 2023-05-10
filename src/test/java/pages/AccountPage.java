75855a2b4a367022d2246984b39ee5e6aa0b6bc3
Federico Goncalvez <129781442+FedericoGoncalvez@users.noreply.github.com>
2023-05-10 14:30:18 -0300

    Refactor: [Change verify element]

--- src/test/java/pages/AccountPage.java
+++ src/test/java/pages/AccountPage.java
@@ -13,19 +13,18 @@ public class AccountPage extends BasePage {
 
     @FindBy(xpath = "//a[@title='Log Out']")
     private WebElement logout;
-    @FindBy(tagName = "h1")
-    private WebElement dashboardTitle;
+
     @FindBy(className = "hello")
     private WebElement loginName;
 
 
 
+    @Step("Se extrae la bienvenida.")
+    public String getTextToDashboard(){
+        return loginName.getText();
+    }
     @Step("Se hace Logout.")
     public void logoutClick(){
         logout.click();
-    }
-    @Step("Se extrae la bienvenida.")
-    public String getTextToDashboard(){
-        return dashboardTitle.getText();
     }
 }
