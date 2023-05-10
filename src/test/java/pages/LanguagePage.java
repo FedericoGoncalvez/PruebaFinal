3ffb4f60d95bc5c68858cfc2300bb7f2d9cb816b
Federico Goncalvez <129781442+FedericoGoncalvez@users.noreply.github.com>
2023-05-10 14:52:10 -0300

    Refactor: [Added language selection logic based on user input.]

--- src/test/java/pages/LanguagePage.java
+++ src/test/java/pages/LanguagePage.java
@@ -15,24 +15,40 @@ public class LanguagePage extends BasePage {
     private WebElement languageDropbox;
     @FindBy(xpath = "//option[contains(text(), 'French')]")
     private WebElement frenchLanguageOption;
+
+    @FindBy(xpath = "//option[contains(text(), 'English')]")
+    private WebElement englishLanguageOption;
+    @FindBy(xpath = "//option[contains(text(), 'German')]")
+    private WebElement germanLanguageOption;
     @FindBy(xpath="//label[text()='Votre Langue:']")
     private WebElement labelLangue;
 
 
 
-    @Step("Se accede a lenguages.")
+    @Step("Se hace clic en lenguages.")
     public void changeLanguage(){
         languageDropbox.click();
     }
 
 
-    @Step("Se selecciona el Frances.")
+    @Step("Se selecciona el idioma .")
     public void selectLanguage(String language) {
-        if (language.equals("French")) {
-            frenchLanguageOption.click();
+        switch (language) {
+            case "French":
+                frenchLanguageOption.click();
+                break;
+            case "English":
+                englishLanguageOption.click();
+                break;
+            case "German":
+                germanLanguageOption.click();
+                break;
+            default:
+                englishLanguageOption.click();
         }
     }
-    @Step("Se hace Logout.")
+
+    @Step("Se verifica el lenguaje.")
     public String getElementText() {
         return labelLangue.getText();
     }
