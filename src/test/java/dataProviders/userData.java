97f5212545dc14c84d333bca1f1472df066f33e8
Federico Goncalvez <129781442+FedericoGoncalvez@users.noreply.github.com>
2023-05-10 14:29:53 -0300

    Refactor: [Change data examples]

--- src/test/java/dataProviders/userData.java
+++ src/test/java/dataProviders/userData.java
@@ -6,8 +6,8 @@ public class userData {
     @DataProvider(name = "userData")
     public static Object[][] getUserData() {
         return new Object[][]{
-                {"xyz@test.com", "123456", "MY DASHBOARD"},
-                {"asd@test.com", "123123", "Invalid login or password."}
+                {"xyz@test.com", "123456", "Hello, holamundo lasttest!"},
+                {"asd@test.com", "123123", "Hello, yzx test!"}
         };
     }
 }
